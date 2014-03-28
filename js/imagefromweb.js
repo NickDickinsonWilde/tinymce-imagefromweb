02a163d1567d668735bf6e9f62a6d3f86807bb8a
Nick Wilde <design@briarmoon.ca>
2014-03-27 18:55:26 -0700

    linted imagesfromweb.js

--- js/imagefromweb.js
+++ js/imagefromweb.js
@@ -1,3 +1,8 @@
+/*jslint browser: true */
+/*jslint plusplus: true */
+/*global parseUri */
+/*global $ */
+/*jslint todo: true */
 function showalert(message, alerttype, linked) {
     "use strict";
     /**
@@ -16,18 +21,18 @@ function showalert(message, alerttype, linked) {
         });
     }
 
-    setTimeout(function() { // this will automatically close the alert and remove this if the users doesn't close it in 5 secs
+    setTimeout(function () { // this will automatically close the alert and remove this if the users doesn't close it in 5 secs
         $("#alertdiv").remove();
         if (linked) {
             $(linked).removeClass("disabled");
-            }
+        }
     }, 3000);
 }
 $("#process").click(function () {
     "use strict";
     var today = new Date(), // today's date - used in the filename.
         src = $("#source_path").val(), //path to the source image
-        dest = $("#dest_path").val(), //filename to save the image 
+        dest = $("#dest_path").val(), //filename to save the image
         file_info = parseUri(src).file.split("."), //the original filename and extension
         funcs = parent.image_from_web_settings.plugin_path + "functions.php", //path to functions.php (which does the heavylifting of copying images).
         credit_link = $("#credit_link").val(), //
@@ -39,7 +44,7 @@ $("#process").click(function () {
     }
     if (dest === "") {
         dest = "%date%-%src_name%.%src_ext%";
-        }
+    }
     if (credit_link === "") {
         credit_link = src;
     }
@@ -48,27 +53,33 @@ $("#process").click(function () {
     }
     if (parent.image_from_web_settings.credit_show === false) {
         credit_msg = "";
+    } else {
+        credit_msg = parent.image_from_web_settings.credit_msg.replace('%link%', "<a href='" + credit_link + "' target='_blank'>" + credit_msg + "</a>");
     }
-    else {
-        credit_msg = parent.image_from_web_settings.credit_msg.replace('%link%', "<a href='" + credit_link +"' target='_blank'>" + credit_msg + "</a>");
-    }
-    dest = parent.image_from_web_settings.upload_path + dest.replace(
-        "%date%", today.toISOString().split("T")[0]).replace(      //replace date placeholder (if present) with today's date
-        "%src_name%",file_info[0]).replace(           //replace name placeholder (if present) with source name
-        "%src_ext%",file_info[1]).replace(/ /g, "-"); //replace extension placeholder (if present) with source's extension
+    dest = parent.image_from_web_settings.upload_path + dest
+        .replace(
+            "%date%",
+            today.toISOString().split("T")[0]
+        ).replace(      //replace date placeholder (if present) with today's date
+            "%src_name%",
+            file_info[0]
+        ).replace(           //replace name placeholder (if present) with source name
+            "%src_ext%",
+            file_info[1]
+        ).replace(/ /g, "-"); //replace extension placeholder (if present) with source's extension
     $.get(funcs, {task: "save_file", src: src, dest: dest, location: funcs}).done(function (data) {
         var imgdata = "<p><img src='" + data + "' />" + credit_msg;
-        if( $("#image").is(':empty')) {
+        if ($("#image").is(':empty')) {
             $("#image").append(imgdata);
             return false;
         }
         $("#image").html("");
         $("#image").append(imgdata);
-        });
+    });
 
 });
 
-$("#image").click(function() {//copy the image code to editor and close dialogue
+$("#image").click(function () {//copy the image code to editor and close dialogue
     "use strict";
     parent.tinymce.activeEditor.insertContent($("#image").html());
     parent.tinymce.activeEditor.windowManager.close();
