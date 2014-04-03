ccc0e61fe998018e6ef9a81471763d0c36b8eaae
Nick Wilde <design@briarmoon.ca>
2014-04-02 17:46:29 -0700

    updated task alliases

--- Gruntfile.js
+++ Gruntfile.js
@@ -156,12 +156,13 @@ module.exports = function (grunt) {
     //Task Groups:
     grunt.registerTask('prebuild', ['set_option:outpath:', 'newer:uncss:main', 'newer:csscomb:main']);
     grunt.registerTask('lint', ['set_option:outpath:', 'newer:jsonlint:main', 'newer:jshint:main']);
-    grunt.registerTask('lint-full', ['set_option:outpath:', 'jsonlint:main', 'jshint:main']);
+    grunt.registerTask('lint_full', ['set_option:outpath:', 'jsonlint:main', 'jshint:main']);
+    grunt.registerTask('minify', ['newer:htmlmin:main', 'newer:imagemin:main', 'newer:uglify:main', 'newer:cssmin:main']);
     grunt.registerTask('dev', ['set_option:outpath:build/dev/', 'newer:copy:main', 'compress:main']);
-    grunt.registerTask('release', ['set_option:outpath:build/release/', 'newer:copy:main', 'newer:processhtml:main', 'newer:htmlmin:main', 'newer:imagemin:main', 'newer:uglify:main', 'newer:cssmin:main', 'compress:main']);
+    grunt.registerTask('release', ['set_option:outpath:build/release/', 'newer:copy:main', 'newer:processhtml:main', 'minify', 'compress:main']);
     grunt.registerTask('build_dev', ['lint', 'prebuild',  'dev']);
     grunt.registerTask('build_release', ['lint', 'prebuild', 'release']);
-    grunt.registerTask('build_dual', ['lint', 'prebuild',  'set_option:outpath:build/dual/', 'newer:uglify:main', 'newer:cssmin:main', 'newer:imagemin:main', 'newer:htmlmin:main', 'newer:copy:main', 'compress']);
+    grunt.registerTask('build_dual', ['lint', 'prebuild',  'set_option:outpath:build/dual/', 'newer:copy:main', 'newer:processhtml:main', 'minify', 'compress:main']);
     grunt.registerTask('default', ['lint', 'prebuild',  'dev', 'release']);
     //cleanup tasks:
     grunt.registerTask('clean_full', ['set_option:outpath:build/', 'clean']);
